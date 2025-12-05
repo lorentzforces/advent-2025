195f7830a01dbf90d4f52b7dda6d09e05befc54d
Lorentz <25649139+lorentzforces@users.noreply.github.com>
2025-12-05 17:17:52 -0600

    day 1 part 1 solution

--- internal/day_01/day_01.go
+++ internal/day_01/day_01.go
@@ -2,11 +2,49 @@ package day_01
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/lorentzforces/advent-2025/internal/puzzle_tools"
 )
 
 func PartOne(input string) (int, error) {
-	puzzle_tools.AsLines(input)
-	return 0, fmt.Errorf("Day 1 part 1 not yet implemented")
+	moves, err := parseMoves(input)
+	if err != nil { return 0, err }
+
+	stopsAtZero := 0
+	dial := 50
+	for _, move := range moves {
+		dial += move
+		if dial % 100 == 0 {
+			stopsAtZero++
+		}
+	}
+
+	return stopsAtZero, nil
+}
+
+func parseMoves(s string) ([]int, error) {
+	lines := puzzle_tools.AsLines(s)
+	moves := make([]int, 0, len(lines))
+
+	for _, line := range lines {
+		runes := []rune(line)
+		var direction int
+		switch runes[0] {
+		case 'L': direction = -1
+		case 'R': direction = 1
+		default: return nil, fmt.Errorf("Could not parse direction for line \"%s\"", line)
+		}
+
+		distance, err := strconv.Atoi(string(runes[1:]))
+		if err != nil {
+			return nil, fmt.Errorf("Error parsing numeric value for line \"%s\": %w", line, err)
+		}
+		if distance < 0 {
+			return nil, fmt.Errorf("Found negative value %d for line \"%s\"", distance, line)
+		}
+
+		moves = append(moves, distance * direction)
+	}
+	return moves, nil
 }
--- internal/day_01/day_01_test.go
+++ internal/day_01/day_01_test.go
@@ -3,10 +3,26 @@ package day_01
 import (
 	"testing"
 
+	"github.com/lorentzforces/advent-2025/internal/puzzle_tools"
 	"github.com/stretchr/testify/assert"
 )
 
+var testInput string =
+`L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+`
+
 func TestPartOneSampleInput(t *testing.T) {
-	_, err := PartOne("")
-	assert.Error(t, err)
+	result, err := PartOne(testInput)
+	assert.NoError(t, err)
+	puzzle_tools.BailIfFailed(t)
+	assert.Equal(t, 3, result)
 }
